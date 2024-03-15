21b2b667c283fb94dacfa05b8adc7d2d016e1189
leghair <zweizhao@foxmail.com>
2024-03-15 16:42:27 +0800

    feat 景区ID参数处理

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,11 +3,24 @@ import { ref, computed } from 'vue'
 import type { Ref } from 'vue'
 import { db } from '@/services/cloud'
 import Map from '@/components/Common/Map.vue'
+import { useRoute } from 'vue-router'
+
+// 景区ID，长河老街?attractionId=65bdf3579beccb0820f313ad
+let id = localStorage.getItem('attractionId') as string
+if (!id) {
+  const { query, params } = useRoute()
+  id = query.attractionId as string
+  if (id) {
+    localStorage.setItem('attractionId', id)
+  } else {
+    throw new Error('进入方式错误，请从官方渠道进入')
+  }
+}
 
 const attraction: Ref<Attraction> = ref(JSON.parse(localStorage.getItem('attraction') as string))
 // 如果依赖用户登录（或匿名登录），则需要监听此事件
 document.addEventListener('login', () => {
-  db.collection('JAttraction').doc('65bdf3579beccb0820f313ad').get()
+  db.collection('JAttraction').doc(id).get()
     .then(({ data }: { data: Attraction[] }) => {
       attraction.value = data[0]
       document.title = attraction.value.name
@@ -19,7 +32,7 @@ document.addEventListener('login', () => {
 
 <template>
   <div class="home-container">
-    <Map />
+    <Map v-if="attraction" />
   </div>
 </template>
 
