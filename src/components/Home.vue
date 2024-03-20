af40023c7a6e2dc6f1474c163f8ebad0cc593a81
leghair <zweizhao@foxmail.com>
2024-03-20 12:18:00 +0800

    feat 针对id的渲染优化

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,19 +3,8 @@ import { ref, computed } from 'vue'
 import type { Ref } from 'vue'
 import { db } from '@/services/cloud'
 import Map from '@/components/Common/Map.vue'
-import { useRoute } from 'vue-router'
 
-// 景区ID，长河老街?attractionId=65bdf3579beccb0820f313ad
 let id = localStorage.getItem('attractionId') as string
-if (!id) {
-  const { query, params } = useRoute()
-  id = query.attractionId as string
-  if (id) {
-    localStorage.setItem('attractionId', id)
-  } else {
-    throw new Error('进入方式错误，请从官方渠道进入')
-  }
-}
 
 const attraction: Ref<Attraction> = ref(JSON.parse(localStorage.getItem('attraction') as string))
 // 如果依赖用户登录（或匿名登录），则需要监听此事件
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,21 @@ import { ref, } from 'vue'
 import InitialForm from '@/components/InitialForm.vue'
 import Tabbar from '@/components/Common/Tabbar.vue'
 import router from '@/router'
+import { useRoute } from 'vue-router'
+
+// 景区ID，长河老街?attractionId=65bdf3579beccb0820f313ad
+let id = ref(localStorage.getItem('attractionId') as string)
+// console.log(id.value)
+if (!id.value) {
+  const { query, params } = useRoute()
+  id.value = query.attractionId as string
+  if (id.value) {
+    localStorage.setItem('attractionId', id.value)
+  } else {
+    alert('进入方式错误，请从官方渠道进入')
+    throw new Error('进入方式错误，请从官方渠道进入')
+  }
+}
 
 const inititalFormed = ref(!!localStorage.getItem('inititalFormed'))
 
@@ -13,11 +28,11 @@ const onChangeTabbarItem = ({ index }: { index: number }) => {
 
 <template>
   <router-view v-slot="{ Component }">
-    <keep-alive>
+    <keep-alive v-if="id">
       <component :is="Component" />
     </keep-alive>
   </router-view>
-  <InitialForm v-if="!inititalFormed" @formed="inititalFormed = true" />
+  <InitialForm v-if="id && !inititalFormed" @formed="inititalFormed = true" />
   <Tabbar @change="onChangeTabbarItem" />
 </template>
 
