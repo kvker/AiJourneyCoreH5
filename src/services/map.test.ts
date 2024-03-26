e5fa0895ba452d7db1bad8b51f66b7d3fd423e1a
leghair <zweizhao@foxmail.com>
2024-03-26 13:30:19 +0800

    test fix map error

--- src/services/map.test.ts
+++ src/services/map.test.ts
@@ -1,17 +1,17 @@
 import { describe, expect, it } from 'vitest'
-import { ll2Lnglat, lnglat2Ll } from './map'
+import Map from './map'
 
 describe('ll2Lnglat', () => {
   it('should convert latitude and longitude to lnglat format', () => {
-    const lnglat = ll2Lnglat({ longitude: -74.0060, latitude: 40.7128 })
+    const lnglat = Map.ll2Lnglat({ lng: -74.0060, lat: 40.7128 })
     expect(lnglat).toEqual({ longitude: -74.0060, latitude: 40.7128 })
   })
 })
 
 describe('lnglat2Ll', () => {
   it('should convert lnglat format to latitude and longitude', () => {
-    const ll = lnglat2Ll({ longitude: -74.0060, latitude: 40.7128 })
-    expect(ll).toEqual({ longitude: -74.0060, latitude: 40.7128 })
+    const ll = Map.lnglat2Ll({ longitude: -74.0060, latitude: 40.7128 })
+    expect(ll).toEqual({ lng: -74.0060, lat: 40.7128 })
   })
 })
 
