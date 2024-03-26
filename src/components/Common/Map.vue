ed99b3718017620b3268e6f60448cd822e0e32d0
leghair <zweizhao@foxmail.com>
2024-03-26 11:50:14 +0800

    feat 地图组件改造非常干净

--- src/components/Common/Map.vue
+++ src/components/Common/Map.vue
@@ -1,12 +1,10 @@
 <script lang="ts" setup>
-import { onMounted, watch, ref, nextTick } from 'vue'
+import { onMounted, watch, ref, nextTick, inject } from 'vue'
 import type { Ref } from 'vue'
 import Map from '@/services/map'
 import type { TmClickEventParams } from '@/services/map'
-import Static from '@/services/static'
 
-import { useAreaList } from '@/composables/areaList'
-import { useMapIntroduce } from '@/composables/mapIntroduce'
+type OverlayItem = ({ lnglat: Lnglat, name: string } & CloudItem)
 
 const props = defineProps({
   defaultLnglat: {
@@ -17,10 +15,26 @@ const props = defineProps({
     type: Object as () => Attraction,
     required: true,
   },
+  overlayList: {
+    type: Array as () => OverlayItem[],
+    default: () => [],
+  }
 })
 
-const { areaList, getAreaList } = useAreaList()
-const { autoPlay, audioRef, currentAudioSrc, onChangeAuto, onAutoPlay, } = useMapIntroduce(areaList)
+// 讲解区域
+const autoPlay = inject('autoPlay') as Ref<boolean>
+
+function onAutoPlay(lnglat: Lnglat) {
+  let minDistance = 20
+  let minItem = props.overlayList[0]
+  props.overlayList.forEach((item) => {
+    const dis = Map.distance(lnglat, item.lnglat)
+    if (dis < minDistance) {
+      minDistance = dis
+      minItem = item
+    }
+  })
+}
 
 const darwMapObject = {
   url: "https://6169-ai-tools-6guwawtsb724a7e7-1254288091.tcb.qcloud.la/images/changhelaojie/handDrawMap.png?sign=0992f6947aba9e9928ecbba881893ab9&t=1710771342",
@@ -39,13 +53,14 @@ onMounted(async () => {
   if (props.defaultLnglat) {
     mapInstance.onAddMarker(props.defaultLnglat)
   }
-  await getAreaList(props.attraction._id)
-  areaList.value.forEach((item: Area) => {
-    mapInstance.onAddLabel({ text: item.name, lnglat: item.lnglat, clickCallback: onClickMapLabel })
-  })
   onEventLoop()
 })
 
+watch(() => props.overlayList, () => {
+  props.overlayList.forEach((item) => {
+    mapInstance.onAddLabel({ text: item.name, lnglat: item.lnglat, clickCallback: onClickMapLabel })
+  })
+})
 
 function onClickMapLabel(e: TmClickEventParams) {
   console.log('点击了地图标签: ', e.target.options.text)
@@ -53,6 +68,7 @@ function onClickMapLabel(e: TmClickEventParams) {
 
 async function onEventLoop() {
   const lnglat = await mapInstance.onAutoLocation()
+  if (autoPlay.value) mapInstance.onSetCenter(lnglat)
   setTimeout(onEventLoop, 10000)
   if (autoPlay.value) onAutoPlay(Map.normalizeLnglat(lnglat))
 }
@@ -63,30 +79,8 @@ async function onEventLoop() {
   <button class="location-button btn btn-primary btn-sm top-8 right-2" :disabled="autoPlay"
     @click="mapInstance.onLocationSelf">
     <img class="local-icon w-4 h-4" src="/icons/local.png" alt="local-icon">
-    同步定位</button>
-  <div class="area-list w-full left-0 bottom-16 translate-y-0 transition-transform"
-    :class="autoPlay ? 'translate-y-48' : 'translate-y-0'">
-    <div class="w-24 bg-white rounded-lg flex flex-col items-center justify-center ml-auto mr-2 mb-2"
-      :class="autoPlay ? 'bottom-64' : 'bottom-20'">
-      <label class="cursor-pointer label">
-        <input type="checkbox" class="toggle toggle-primary" @change="onChangeAuto" />
-      </label>
-      <p class="text-xs">自动讲解</p>
-    </div>
-    <ul class="bg-white w-full h-48 overflow-y-scroll">
-      <li v-for="item in areaList" :key="item._id" class="flex items-center justify-between p-2">
-        <img class="w-12 h-12 rounded-full" :src="Static.miniImage(item.coverImageList[0])" alt="cover-image">
-        <section class="info flex-1 ml-2">
-          <h3 class=" text-base">{{ item.name }}</h3>
-          <p class=" text-gray-400 text-xs">{{ item.name }}</p>
-        </section>
-        <img class=" w-6 h-6" :src="item.playState === 'ended' ? '/icons/audio.png' : '/icons/audio-disabled.png'"
-          alt="audio">
-      </li>
-    </ul>
-  </div>
-  <audio ref="audioRef" :src="currentAudioSrc" class="audio-player fixed left-full"></audio>
-
+    同步定位
+  </button>
 </template>
 
 <style scoped>
